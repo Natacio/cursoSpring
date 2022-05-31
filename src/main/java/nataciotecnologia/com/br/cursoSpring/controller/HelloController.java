3f0aaa83b0425adc1f5e8597168d414dc9e4a791
Natacio <natacio07@gmail.com>
2022-05-31 14:32:01 -0300

    new project

---
+++ src/main/java/nataciotecnologia/com/br/cursoSpring/controller/HelloController.java
@@ -0,0 +1,8 @@
+package nataciotecnologia.com.br.cursoSpring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+}
---
+++ src/test/java/nataciotecnologia/com/br/cursoSpring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package nataciotecnologia.com.br.cursoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
